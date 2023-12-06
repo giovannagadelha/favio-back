a0dec28cf42024b84738874ff478ccb80bbbeb5d
Giovanna Liz <gigilizgadelha@gmail.com>
2023-12-06 11:20:48 -0300

    atualizado

--- tests/functional/atualizar_favorito.spec.ts
+++ tests/functional/atualizar_favorito.spec.ts
@@ -1,5 +1,10 @@
 import { test } from '@japa/runner'
 
 test.group('Atualizar favorito', () => {
- test('atualizar favorito', async )
+ test('atualizar favorito', async ({ client }) => {
+  const favoritoId = 1;
+  
+
+ }
+ )
 })
